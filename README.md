f2d64ef500864fa757052b04ffbfdc31ae0d5b92
owpk <vzvz4@mail.ru>
2021-10-21 14:02:24 +0800

    java 1.8

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ repositories {
 }
 
 dependencies {
-   implementation 'com.github.owpk:ANSIJava:1.5'
+   implementation 'com.github.owpk:ANSIJava:1.7'
    // or 'com.github.owpk:ANSIJava:master-SNAPSHOT'
 }
 ```
--- src/main/java/ansi/ANSIJava.java
+++ src/main/java/ansi/ANSIJava.java
@@ -39,7 +39,7 @@ public final class ANSIJava {
     }
 
     private static boolean checkForEmptyString(String input) {
-        return input != null && !input.isBlank();
+        return input != null && !input.equals("");
     }
 
     private static boolean checkForEmptyList(List<String> list) {
